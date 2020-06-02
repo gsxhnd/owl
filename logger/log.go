383087e438829f3a049b11f7b617cb6e56aa7a6f
gsxhnd <gsxhnd@gmail.com>
2020-06-02 14:17:25 +0800

    chore: update dep

--- logger/log.go
+++ logger/log.go
@@ -1,10 +1,8 @@
 package logger
 
 import (
-	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"io"
 	"os"
 	"time"
 )
@@ -52,12 +50,9 @@ func init() {
 		return lvl >= zapcore.WarnLevel
 	})
 
-	infoHook := getWriter("./log/demo")
-	warnHook := getWriter("./log/demo_error")
-
 	core := zapcore.NewTee(
-		zapcore.NewCore(proEncoder, zapcore.AddSync(infoHook), infoLevel),
-		zapcore.NewCore(proEncoder, zapcore.AddSync(warnHook), warnLevel),
+		zapcore.NewCore(proEncoder, os.Stdout, infoLevel),
+		zapcore.NewCore(proEncoder, os.Stdout, warnLevel),
 		zapcore.NewCore(debugEncoder, os.Stdout, zap.DebugLevel),
 	)
 	logger = zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
@@ -84,23 +79,6 @@ type Level int8
 //func SetLevel(level Level) {
 //	logLevel.SetLevel(zapcore.Level(level))
 //}
-
-func getWriter(filename string) io.Writer {
-	// 生成rotatelogs的Logger 实际生成的文件名 demo.log.YYmmddHH
-	// demo.log是指向最新日志的链接
-	// 保存7天内的日志，每1小时(整点)分割一次日志
-	hook, err := rotatelogs.New(
-		filename+".%Y%m%d.log", // 没有使用go风格反人类的format格式
-		//rotatelogs.WithLinkName(filename),
-		rotatelogs.WithMaxAge(time.Hour*24),
-		rotatelogs.WithRotationTime(time.Hour*24),
-	)
-
-	if err != nil {
-		panic(err)
-	}
-	return hook
-}
 
 func Debug(msg string, fields ...zap.Field) {
 	logger.Debug(msg, fields...)
