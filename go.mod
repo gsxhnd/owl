9267305a1c6d0e19f6caeca9acc9e815b57238d1
gsxhnd <gsxhnd@gmail.com>
2020-09-27 13:51:40 +0800

    test: add test

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/coreos/etcd v3.3.25+incompatible
 	github.com/gsxhnd/cast v1.3.2-0.20200903083226-662305f032b9
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.6.1
 	go.etcd.io/etcd v0.0.0-20200824191128-ae9734ed278b // indirect
-	gopkg.in/yaml.v2 v2.3.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
--- owl_test.go
+++ owl_test.go
@@ -1,58 +1,89 @@
 package owl
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
 
+var exampleYaml = `
+name: test
+addr: :8080
+test:
+  test01: test01
+  test02: test02
+`
+
+var exampleByteYaml = []byte(exampleYaml)
+
+var emptyByteYaml = []byte("")
+
 func TestNew(t *testing.T) {
 	owlTest := New()
-	t.Log(&owlTest, &owl)
-	if &owlTest == &owl {
-		t.Error("use one instance")
-	}
+	assert.NotNil(t, owlTest)
+	assert.NotSame(t, owlTest, owl)
 }
 
 func TestAddConfPath(t *testing.T) {
 	AddConfPath("1")
 	AddConfPath("2")
-	if len(owl.filepath) != 2 {
-		t.Error("length error")
-	}
-	if owl.filepath[0] != "1" || owl.filepath[1] != "2" {
-		t.Error("filepath value error")
-	}
+	assert.Equal(t, 2, len(owl.filepath))
+	assert.Equal(t, "1", owl.filepath[0])
+	assert.Equal(t, "2", owl.filepath[1])
 }
 
 func TestSetConfName(t *testing.T) {
 	SetConfName("test.yaml")
-	if owl.filename == "" {
-		t.Error("config name is not set")
-	}
-	if owl.filename != "test.yaml" {
-		t.Error("config name is error")
-	}
+	assert.NotEmpty(t, owl.filename)
+	assert.Equal(t, "test.yaml", owl.filename)
 }
 
 func TestReadConf(t *testing.T) {
-	SetConfName("test.yaml")
 	AddConfPath("./mock/")
+	SetConfName("test.yaml")
 	err := ReadConf()
-	if err != nil {
-		t.Error(fmt.Sprintf("%+v", err))
+	assert.Nil(t, err)
+	SetConfName("test1.yaml")
+	err = ReadConf()
+	assert.Error(t, err)
+}
+
+func TestReadInConf(t *testing.T) {
+	type args struct {
+		content []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "", args: args{content: exampleByteYaml}, wantErr: false},
 	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ReadInConf(tt.args.content); (err != nil) != tt.wantErr {
+				t.Errorf("ReadInConf() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestSetRemoteAddr(t *testing.T) {
+	SetRemoteAddr([]string{"localhost:2379"})
+	assert.NotNil(t, owl.client)
 }
 
 func TestGetAll(t *testing.T) {
-	SetConfName("test.yaml")
 	AddConfPath("./mock/")
+
+	SetConfName("test.yaml")
 	_ = ReadConf()
-	conf := GetAll()
-	if conf == nil {
-		t.Error("conf is nil")
-	}
+	assert.NotNil(t, GetAll())
+
+	owl.config = nil
+	_ = ReadInConf(emptyByteYaml)
+	assert.Nil(t, GetAll())
 }
 
 func TestGet(t *testing.T) {
