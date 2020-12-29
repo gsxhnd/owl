f43818cbd30ef0ecd4c750cbb7290578c5967768
gsxhnd <gsxhnd@gmail.com>
2020-12-29 15:03:20 +0800

    test: update unit test

--- owl_test.go
+++ owl_test.go
@@ -13,9 +13,19 @@ test:
   test02: test02
 `
 
-var exampleByteYaml = []byte(exampleYaml)
-
-var emptyByteYaml = []byte("")
+var exampleErrYaml = `
+name: test
+addr: :8080
+test:
+  test01: test01
+  test02: test02
+  ---
+`
+var (
+	exampleByteYaml    = []byte(exampleYaml)
+	exampleErrByteYaml = []byte(exampleErrYaml)
+	emptyByteYaml      = []byte("")
+)
 
 func resetOwl() {
 	owl.filename = ""
@@ -38,8 +48,8 @@ func TestSetRemoteAddr(t *testing.T) {
 		addr    []string
 		wantErr bool
 	}{
-		{"not_err", []string{"localhost:2379"}, false},
-		{"err", []string{"192.168.1.1:2379"}, true},
+		{"success", []string{"localhost:2379"}, false},
+		{"fail", []string{"192.168.1.1:2379"}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -47,30 +57,7 @@ func TestSetRemoteAddr(t *testing.T) {
 			err := SetRemoteAddr(tt.addr)
 			if tt.wantErr {
 				assert.Error(t, err)
-			} else {
-				assert.Nil(t, err)
-			}
-		})
-	}
-	err := SetRemoteAddr([]string{"localhost:2379"})
-	assert.NotNil(t, owl.client)
-	assert.Nil(t, err)
-}
-func TestOwl_SetRemoteAddr(t *testing.T) {
-	tests := []struct {
-		name    string
-		addr    []string
-		wantErr bool
-	}{
-		{"not_err", []string{"localhost:2379"}, false},
-		{"err", []string{"192.168.1.1:2379"}, true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			err := o.SetRemoteAddr(tt.addr)
-			if tt.wantErr {
-				assert.Error(t, err)
+				assert.NotNil(t, owl.client)
 			} else {
 				assert.Nil(t, err)
 			}
@@ -91,28 +78,6 @@ func TestPutRemote(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			_ = SetRemoteAddr([]string{"localhost:2379"})
 			err := PutRemote(tt.key, tt.value)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.Nil(t, err)
-			}
-		})
-	}
-}
-func TestOwl_PutRemote(t *testing.T) {
-	tests := []struct {
-		name    string
-		key     string
-		value   string
-		wantErr bool
-	}{
-		{"test_success", "/test", "test", false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			_ = o.SetRemoteAddr([]string{"localhost:2379"})
-			err := o.PutRemote(tt.key, tt.value)
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
@@ -143,28 +108,6 @@ func TestGetRemoteKeys(t *testing.T) {
 		})
 	}
 }
-func TestOwl_GetRemoteKeys(t *testing.T) {
-	tests := []struct {
-		name    string
-		prefix  string
-		want    []string
-		wantErr bool
-	}{
-		{"test", "/test", []string{"/test"}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			_ = o.SetRemoteAddr([]string{"localhost:2379"})
-			keys, err := o.GetRemoteKeys(tt.prefix)
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.Equal(t, tt.want, keys)
-			}
-		})
-	}
-}
 
 func TestGetRemote(t *testing.T) {
 	tests := []struct {
@@ -187,30 +130,6 @@ func TestGetRemote(t *testing.T) {
 		})
 	}
 }
-
-//func TestOwl_GetRemote(t *testing.T) {
-//	tests := []struct {
-//		name    string
-//		key     string
-//		want    string
-//		wantErr bool
-//	}{
-//		{"test_success", "/test", "test", false},
-//		{"test_nill", "/test_nil", "", false},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			o := New()
-//			_ = o.SetRemoteAddr([]string{"localhost:2379"})
-//			v, err := o.GetRemote(tt.key)
-//			if tt.wantErr {
-//				assert.Error(t, err)
-//			} else {
-//				assert.Equal(t, tt.want, v)
-//			}
-//		})
-//	}
-//}
 
 func TestDeleteRemote(t *testing.T) {
 	tests := []struct {
@@ -257,32 +176,6 @@ func TestWatcher(t *testing.T) {
 		})
 	}
 }
-func TestOwl_Watcher(t *testing.T) {
-	tests := []struct {
-		name string
-		key  string
-		want string
-	}{
-		{"test_watch", "/t1", "t1"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			_ = o.SetRemoteAddr([]string{"localhost:2379"})
-			c := make(chan string)
-			var s string
-			go o.Watcher(tt.key, c)
-			go func() {
-				select {
-				case s = <-c:
-					assert.Equal(t, tt.want, s)
-				}
-			}()
-			_ = o.PutRemote(tt.key, tt.want)
-
-		})
-	}
-}
 
 func TestAddConfPath(t *testing.T) {
 	AddConfPath("1")
@@ -290,14 +183,6 @@ func TestAddConfPath(t *testing.T) {
 	assert.Equal(t, 2, len(owl.filepath))
 	assert.Equal(t, "1", owl.filepath[0])
 	assert.Equal(t, "2", owl.filepath[1])
-}
-func TestOwl_AddConfPath(t *testing.T) {
-	o := New()
-	o.AddConfPath("1")
-	o.AddConfPath("2")
-	assert.Equal(t, 2, len(o.filepath))
-	assert.Equal(t, "1", o.filepath[0])
-	assert.Equal(t, "2", o.filepath[1])
 }
 
 func TestSetConfName(t *testing.T) {
@@ -315,24 +200,6 @@ func TestSetConfName(t *testing.T) {
 			SetConfName(tt.value)
 			assert.NotEmpty(t, owl.filename)
 			assert.Equal(t, owl.filename, tt.want)
-		})
-	}
-}
-func TestOwl_SetConfName(t *testing.T) {
-	tests := []struct {
-		name  string
-		value string
-		want  string
-	}{
-		{"a", "test.yaml", "test.yaml"},
-		{"b", "test1.yaml", "test1.yaml"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			o.SetConfName(tt.value)
-			assert.NotEmpty(t, o.filename)
-			assert.Equal(t, o.filename, tt.want)
 		})
 	}
 }
@@ -363,66 +230,20 @@ func TestReadConf(t *testing.T) {
 		})
 	}
 }
-func TestOwl_ReadConf(t *testing.T) {
-	tests := []struct {
-		name     string
-		path     string
-		filename string
-		wantErr  bool
-	}{
-		{"a", "./mock/", "test.yaml", false},
-		{"b", "./mock/", "test1.yaml", true},
-		{"c", "./mock/", "", true},
-		{"d", "", "test1.yaml", true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			o.AddConfPath(tt.path)
-			o.SetConfName(tt.filename)
-			err := o.ReadConf()
-			if tt.wantErr {
-				assert.Error(t, err)
-			} else {
-				assert.Nil(t, err)
-			}
-		})
-	}
-}
 
 func TestReadInConf(t *testing.T) {
-	type args struct {
-		content []byte
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{name: "", args: args{content: exampleByteYaml}, wantErr: false},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := ReadInConf(tt.args.content); (err != nil) != tt.wantErr {
-				t.Errorf("ReadInConf() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-func TestOwl_ReadInConf(t *testing.T) {
 	tests := []struct {
 		name    string
 		content []byte
 		wantErr bool
 	}{
-		{"", exampleByteYaml, false},
+		{"success", exampleByteYaml, false},
+		{"success", exampleErrByteYaml, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			o := New()
-			err := o.ReadInConf(tt.content)
+			err := ReadInConf(tt.content)
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
